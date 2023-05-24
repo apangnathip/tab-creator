d15f6e4f6bf126d2d38f0e8e077aa7479c94a8ff
Alan Pangnathip <alanpangnathip@gmail.com>
2023-05-24 19:35:05 -0400

    Implement local storage for theme toggler

--- src/contexts/ThemeContext.tsx
+++ src/contexts/ThemeContext.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, ReactNode, SetStateAction, createContext, useState } from "react";
+import { Dispatch, ReactNode, SetStateAction, createContext, useEffect, useState } from "react";
 
 type Theme = "light" | "dark";
 type ThemeContext = {
@@ -9,9 +9,23 @@ type ThemeContext = {
 
 export const ThemeContext = createContext({} as ThemeContext);
 
+function getTheme() {
+  const theme = localStorage.getItem("theme");
+  if (theme) {
+    return theme as Theme;
+  } else {
+    localStorage.setItem("theme", "light");
+    return "light" as Theme;
+  }
+}
+
 export function ThemeProvider({ children }: { children: ReactNode }) {
-  const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState(getTheme());
   const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+  }, [theme]);
 
   return <ThemeContext.Provider value={{ theme, setTheme, toggleTheme }}>{children}</ThemeContext.Provider>;
 }
